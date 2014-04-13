2b3ceae34908ce333fdcc717707fe99250ecf41a
James Eagle <jimeage@gmail.com>
2014-04-13 22:54:50 +0100

    Throw some exceptions

--- src/Jleagle/PHPSickBeard/PHPSickBeard.php
+++ src/Jleagle/PHPSickBeard/PHPSickBeard.php
@@ -15,18 +15,32 @@ class PHPSickBeard
 
     public function __call($method, $parameters = array())
     {
+        $parameters = $parameters[0];
         $method = str_replace('_', '.', $method);
         $parameters['cmd'] = $method;
-        return $this->request($method, $parameters);
+        return $this->request($parameters);
     }
 
     private function request($parameters = array()){
 
+        if (empty($parameters)){
+            throw new \Exception("No parameters specified.");
+        }
+
         $url = $this->url.'/api/'.$this->apiKey.'/?';
         $query = http_build_query($parameters);
         $json = file_get_contents($url.$query);
-        $json = json_decode($json, true);
-        return $json;
+        $array = json_decode($json, true);
+
+        if (!$array){
+            throw new \Exception('Invalid response');
+        }
+
+        if ($array['result'] != 'success'){
+            throw new \Exception($array['message']);
+        }
+
+        return $array;
 
     }
 
