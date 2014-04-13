f27722cd92aeb1136006d9166086979d9fd980bc
James Eagle <jimeage@gmail.com>
2014-04-13 21:08:22 +0100

    Add the functions

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ php-sick-beard
 ==============
 
 A wrapper class for Sick Beard
+
+
+Installation
+
+Use Composer to install by adding
+
+"require": {
+    "jleagle/php-sick-beard": "dev-master"
+},
+
+```php
+// The secret is only needed if you want to access authenticated methods
+$lastfm = new \Dandelionmood\LastFm\LastFm( $lastfm_api_key, $lastfm_api_secret );
+```
--- src/Jleagle/PHPSickBeard/PHPSickBeard.php
+++ src/Jleagle/PHPSickBeard/PHPSickBeard.php
@@ -3,8 +3,31 @@ namespace Jleagle\PHPSickBeard;
 
 class PHPSickBeard
 {
-    public function index()
+
+    private $url;
+    private $apiKey;
+
+    public function __construct($url, $apiKey)
     {
-        return 'Hello World';
+        $this->url = $url;
+        $this->apiKey = $apiKey;
     }
+
+    public function __call($method, $parameters = array())
+    {
+        $method = str_replace('_', '.', $method);
+        $parameters['cmd'] = $method;
+        return $this->request($method, $parameters);
+    }
+
+    private function request($parameters = array()){
+
+        $url = $this->url.'/api/'.$this->apiKey.'/?';
+        $query = http_build_query($parameters);
+        $json = file_get_contents($url.$query);
+        $json = json_decode($json, true);
+        return $json;
+
+    }
+
 }
