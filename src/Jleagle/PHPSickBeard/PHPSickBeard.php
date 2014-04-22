f446186691b80e5235b84830c4cdd1923b3643cc
James Eagle <jimeagle@gmail.com>
2014-04-22 23:28:07 +0100

    Use Guzzle

--- src/Jleagle/PHPSickBeard/PHPSickBeard.php
+++ src/Jleagle/PHPSickBeard/PHPSickBeard.php
@@ -1,6 +1,8 @@
 <?php
 namespace Jleagle\PHPSickBeard;
 
+use \GuzzleHttp\Client;
+
 class PHPSickBeard
 {
 
@@ -24,33 +26,57 @@ class PHPSickBeard
 
     }
 
-    private function request($parameters = array()){
+    private function request($parameters = array())
+    {
 
-        if (empty($parameters)){
+        if (empty($parameters))
+        {
             throw new \Exception("No parameters specified.");
         }
 
-        if (empty($this->url) || empty($this->apiKey)){
+        if (empty($this->url) || empty($this->apiKey))
+        {
             throw new \Exception("No api key or URL set.");
         }
 
+        // Build URL
         $url = $this->url.'/api/'.$this->apiKey.'/?';
         $query = http_build_query($parameters);
-        $json = file_get_contents($url.$query);
-        $array = json_decode($json, $this->arrayBase);
 
-        if (!$array){
+        // Guzzle
+        $client = new Client();
+        $response = $client->get($url.$query);
+
+        if ($response->getStatusCode() != 200)
+        {
             throw new \Exception('Invalid response');
         }
 
-        if ($array['result'] != 'success'){
-            if (empty($array['message'])){
-                $array['message'] = 'Error';
-            }
-            throw new \Exception($array['message']);
-        }
+        $body = $response->getBody();
 
-        return $array['data'];
+        if (strpos($response->getHeader('content-type'), 'json') !== false)
+        {
+            $array = json_decode($body, $this->arrayBase);
+
+            if (!$array)
+            {
+                throw new \Exception('Invalid response');
+            }
+
+            if (isset($array['result']) && $array['result'] != 'success')
+            {
+                if (empty($array['message'])){
+                    $array['message'] = 'Error';
+                }
+                throw new \Exception($array['message']);
+            }
+
+            return $array['data'];
+        }
+        else
+        {
+            return $body;
+        }
 
     }
 
